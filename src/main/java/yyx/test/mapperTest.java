5360fff8d472750e203a470860d4607657875bb7
yux20000304 <43885995+yux20000304@users.noreply.github.com>
2021-06-14 21:34:16 +0800

    开始添加sql语句

--- src/main/java/yyx/test/mapperTest.java
+++ src/main/java/yyx/test/mapperTest.java
@@ -4,12 +4,18 @@ package yyx.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import yyx.dao.ManagerMapper;
 
 
 //使用注解进行单元测试
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class mapperTest {
+    public static void main(String argc[]){
+        ApplicationContext ioc=new ClassPathXmlApplicationContext("applicationContext.xml");
+        ManagerMapper managerMapper=ioc.getBean(ManagerMapper.class);
+        System.out.println(managerMapper);
+    }
 }
